64ad60641b67e163027ef8fe920365f9854be87d
average-alien <allentang113@gmail.com>
2022-09-23 15:18:34 -0700

    finishes part 2

--- src/MyList.js
+++ src/MyList.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react'
+import './App.css'
 import ListItem from './ListItem'
 
 class MyList extends Component {
+  state = {
+    taskArray: this.props.theList,
+    newItem: ''
+  }
+
+  clearList = () => {
+    console.log("Clearing list!")
+
+    this.setState({
+      taskArray: []
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    this.setState(prevState => {
+      return {
+        taskArray: [...prevState.taskArray, prevState.newItem],
+        newItem: ''
+      }
+    })
+    console.log(this.state)
+  }
+
   render() {
-    const toDoItems = this.props.theList.map((item, i) => {
+    const toDoItems = this.state.taskArray.map((item, i) => {
       return <ListItem task={item} key={i} />
     })
+
     return(
-      <div>
+      <>
         <h1>Things I should Stop procrastinating:</h1>
+
+        <form onSubmit={this.addItem}>
+          <label htmlFor='newItem' >New To Do:</label>
+          <input type='text'
+            placeholder='Type an item here'
+            onChange={this.handleChange}
+            value={this.state.newItem}
+          />
+          <button type='submit'>Add to List!</button>
+        </form>
 
         <ul>
           {toDoItems}
         </ul>
-      </div>
+
+        <button onClick={this.clearList}>Finished the list!</button>
+      </>
     )
   }
 }
